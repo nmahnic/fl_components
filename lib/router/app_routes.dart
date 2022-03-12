35ef7e686306a02727dbf63117da4d23caa32b58
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-03-12 12:18:28 -0300

    update routes

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -7,8 +7,6 @@ class AppRoutes {
 
   static const initialRoute = '/home';
   static final menuOptions = <MenuOption> [
-    // TODO: borrar home
-    MenuOption(name: 'Home Screen', route: '/home', screen: const HomeScreen(), icon: Icons.home),
     MenuOption(name: 'List1 Screen', route: '/listView1', screen: const ListView1Screen(), icon: Icons.list),
     MenuOption(name: 'List2 Screen', route: '/listView2', screen: const ListView2Screen(), icon: Icons.list_alt),
     MenuOption(name: 'Card Screen', route: '/card', screen: const CardScreen(), icon: Icons.card_giftcard),
@@ -22,6 +20,8 @@ class AppRoutes {
   static  Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
 
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen());
+    
     for (final option in menuOptions) {
       appRoutes.addAll({option.route : (BuildContext context) => option.screen});
     }
